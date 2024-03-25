e8a0220b466ff534d8eaa6e4aaa7d0652d1999e2
Abdullah Harits <harits.abdullah19@gmail.com>
2024-03-25 19:59:57 +0700

    update init mongo script

--- init_mongodb.sh
+++ init_mongodb.sh
@@ -1,13 +1,10 @@
-MONGO_VERSION=6.0
-sudo apt-get update
-sudo apt-get install gnupg -y
-# Changes here!
-  sudo mkdir -p /etc/apt/keyrings
-  curl -fsSL https://www.mongodb.org/static/pgp/server-${MONGO_VERSION}.asc | sudo gpg --dearmor -o /etc/apt/keyrings/mongodb-${MONGO_VERSION}.gpg
-cd /etc/apt/sources.list.d/
-sudo touch mongodb-org-${MONGO_VERSION}.list
-# And here!
-  echo "deb [arch=amd64,arm64 signed-by=/etc/apt/keyrings/mongodb-${MONGO_VERSION}.gpg] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/${MONGO_VERSION} multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-${MONGO_VERSION}.list
+sudo apt-get install gnupg curl
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+sudo systemctl daemon-reload
 sudo systemctl start mongod
+sudo systemctl status mongod
